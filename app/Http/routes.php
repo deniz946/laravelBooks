47b9ccc90ed7d023b8e9ac2de4b1e91122aa8964
deniz946 <deniz946@gmail.com>
2016-07-13 20:10:52 +0200

    zz

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,31 +12,33 @@
 */
 
 
-Route::get('/', 'BooksController@index');
+Route::group(['prefix'] => 'lara/public', function(){
+	Route::get('/', 'BooksController@index');
 
-Route::resource('books', 'BooksController');
+	Route::resource('books', 'BooksController');
 
-Route::group(['prefix' => 'admin', "middleware" => 'auth'], function(){
+	Route::group(['prefix' => 'admin', "middleware" => 'auth'], function(){
 
-	Route::get('/', 'AdminController@index');
+		Route::get('/', 'AdminController@index');
 
-	//ROLES ROUTES
-	Route::resource('roles', 'RolesController');
+		//ROLES ROUTES
+		Route::resource('roles', 'RolesController');
 
-	//PERMISSIONS ROUTES
-	Route::resource('permissions', 'PermissionsController');
+		//PERMISSIONS ROUTES
+		Route::resource('permissions', 'PermissionsController');
 
-	//USERS ROUTES
-	Route::resource('users', 'UsersController');
-	Route::get('/users/profile', 'UsersController@profile');
-});
+		//USERS ROUTES
+		Route::resource('users', 'UsersController');
+		Route::get('/users/profile', 'UsersController@profile');
+	});
 
-Route::group(['prefix' => 'users', 'middleware' => 'auth'], function(){
-	
-	Route::get('profile', ['uses' => 'UsersController@profile',
-		'as' => 'users.profile']);
-});
+	Route::group(['prefix' => 'users', 'middleware' => 'auth'], function(){
+		
+		Route::get('profile', ['uses' => 'UsersController@profile',
+			'as' => 'users.profile']);
+	});
 
 
-Route::auth();
+	Route::auth();
+})
 
